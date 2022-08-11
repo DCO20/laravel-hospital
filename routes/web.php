d3445d0dd62858cb41599c12c06693206989de5b
Danilo Oliveira <cardoso.1007@hotmail.com>
2022-08-11 10:53:05 -0300

    pacient - adicionado index

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Pacient\Index;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +26,6 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('/pacientes', Index::class)->name('pacient.index');
 });
