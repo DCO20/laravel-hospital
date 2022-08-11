06739d6cd79b1d764dba6b1f08608f5dbdc8b924
Danilo Oliveira <cardoso.1007@hotmail.com>
2022-08-11 10:52:37 -0300

    pacient - adicionado migração e entidade

---
+++ app/Models/Pacient.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Pacient extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * Tabela do banco de dados
+     *
+     * @var string
+     */
+    protected $table = 'pacients';
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'document',
+        'phone',
+        'street',
+        'number',
+        'description',
+    ];
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+}
