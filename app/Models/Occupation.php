4d35112c2b73869f8bb39059a0c336163f11c505
Danilo Oliveira <cardoso.1007@hotmail.com>
2022-08-12 10:41:53 -0300

    occupation - adicionado migração e entidade

---
+++ app/Models/Occupation.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Occupation extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * Tabela do banco de dados
+     *
+     * @var string
+     */
+    protected $table = 'occupations';
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    /**
+     * Atributos da tabela do banco de dados
+     *
+     *  @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+}
